6a840f10607751ca73e8a0e962a65f7c0f413c56
你的 Github 用户名 < Github >
2024-08-09 09:26:46 +0800

    player&重构

---
+++ bin/Players.h
@@ -0,0 +1,159 @@
+#ifndef Players_H
+#define Players_H
+#include <windows.h>
+#include <bits/stdc++.h>
+#include "MiniLinux.h"
+using namespace std;
+
+const string serial_num[10]={"0","①","②","③","④","⑤","⑥","⑦","⑧","⑨"};
+
+class Player
+{
+public:
+    int id;
+    int current_index;
+    int folder_tot=0;
+    list<string> cards;
+    deque<int> path;
+    Player(){}
+    Player(int _id,directory dire)
+    {
+        id=_id;
+        player_init_directory(dire);
+        player_init_files(dire);
+    }
+    void display_current_directory(directory dire)
+    {
+        cout<<dire.name[current_index]<<endl;
+        for(int i=0;i<dire.dire[current_index].size();i++)
+        {
+            if(i==dire.dire[current_index].size()-1)
+            {
+                cout<<"└──"<<serial_num[i+1]<<dire.name[dire.dire[current_index][i]]<<endl;
+            }
+            else
+            {
+                cout<<"├──"<<serial_num[i+1]<<dire.name[dire.dire[current_index][i]]<<endl;
+            }
+        }
+    }
+    void player_init_directory(directory dire)
+    {
+        string temp="player";
+        temp+='0'+id;
+        cout<<temp<<endl;
+        current_index=dire.mkdir(1,temp);
+        path.push_back(1);
+        path.push_back(current_index);
+    }
+    void player_init_files(directory dire)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            string temp;
+            temp+="player";
+            temp+=to_string(id);
+            temp+="_file";
+            temp+=('0'+j);
+            dire.echo(id+1,id,temp);
+        }
+    }
+    void load_cards(ifstream& infile,int num)
+    {
+        int n,m;
+        infile>>n>>m;
+        for(int i=1;i<=num;i++)
+        {
+            int temp=rand()%(m+1)+1;
+            for(int j=1;j<=n;j++)
+            {
+                int l,r;
+                string name;
+                infile>>l>>r>>name;
+                if(l<=temp&&temp<=r)
+                {
+                    cards.push_back(name);
+                }
+            }
+        }
+    }
+    void display(directory dire)
+    {
+        dire.print();
+        cout<<id<<" 号玩家当前路径: "<<endl;
+        cout<<"Home";
+        for(int j=1;j<path.size();j++)
+        {
+            cout<<"/"<<dire.name[path[j]];
+        }
+        cout<<endl;
+
+        cout<<id<<" 号玩家的手牌: "<<endl;
+        for(auto j=cards.begin();j!=cards.end();j++)
+        {
+            cout<<"  "<<*j<<endl;
+        }
+    }
+    //card
+    void cd(directory dire)
+    {
+        auto t=find(cards.begin(),cards.end(),"cd");
+        if(t!=cards.end())
+        {
+            cards.erase(t);
+            display_current_directory(dire);
+            while(1)
+            {
+                cout<<"请选择路径编号（如要后退输入..）：";
+                string temp;
+                cin>>temp;
+                if(temp=="..")
+                {
+                    path.pop_back();
+                    break;
+                }
+                int folder_id=temp[0]-'0';
+                if(folder_id>dire.dire[current_index].size())
+                {
+                    wostringstream temp_massage;
+                    temp_massage<<L"没有这个路径哦";
+                    wostringstream temp_title;
+                    temp_title<<id<<L" 号玩家出牌阶段";
+                    MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
+                }
+                path.push_back(dire.dire[current_index][folder_id-1]);
+                current_index=dire.dire[current_index][folder_id-1];
+                break;
+            }
+        }
+        else
+        {
+            wostringstream temp_massage;
+            temp_massage<<L"您没有 cd 这张牌哦";
+            wostringstream temp_title;
+            temp_title<<id<<L" 号玩家出牌阶段";
+            MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
+        }
+    }
+
+    void mkdir(directory dire)
+    {
+        auto t=find(cards.begin(),cards.end(),"mkdir");
+        if(t!=cards.end())
+        {
+            cards.erase(t);
+            string temp="folder";
+            temp+='0'+(++folder_tot);
+            dire.mkdir(current_index,temp);
+        }
+        else
+        {
+            wostringstream temp_massage;
+            temp_massage<<L"您没有 mkdir 这张牌哦";
+            wostringstream temp_title;
+            temp_title<<id<<L" 号玩家出牌阶段";
+            MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
+        }
+    }
+};
+#endif
--- game.cpp
+++ game.cpp
@@ -1,172 +1,58 @@
 #include <bits/stdc++.h>
 #include <windows.h>
 #include "bin/MiniLinux.h"
+#include "bin/Players.h"
 using namespace std;
 
-const string serial_num[10]={"0","①","②","③","④","⑤","⑥","⑦","⑧","⑨"};
-
 int player_sum=4;//player sum
-list<string> player_card[5];//players' card
-int player_index[5];//players' index
-deque<int> player_path[5];//players' path
 directory dire;// directory
-int folder_tot=0;
+Player players[5];
 
 void init()//init directory
 {
     dire.name[1]="Home";
     for(int i=1;i<=4;i++)
     {
-        string temp;temp="player";temp+='0'+i;
-        cout<<temp<<endl;
-        int id=dire.mkdir(1,temp);//players' fordle
-        player_index[i]=id;
-        player_path[i].push_back(1);
-        player_path[i].push_back(id);
-    }
-    for(int i=1;i<=4;i++)
-    {
-        for(int j=1;j<=5;j++)
-        {
-            string temp;
-            temp+="player";temp+=(char)('0'+i);temp+="_file";temp+=('0'+j);
-            dire.echo(i+1,i,temp);//players' file
-        }
+        players[i]=Player(i,dire);
     }
 }
 
 void dispense()//dispense card
 {
+    ifstream infile("_data/_card.txt");
     for(int i=1;i<=4;i++)
     {
-        ifstream infile("_data/_card.txt");
-        for(int i=1;i<=4;i++)
-        {
-            int n,m;
-            infile>>n>>m;
-            int id=rand()%(m+1)+1;
-            for(int j=1;j<=n;j++)
-            {
-                int l,r;
-                string name;
-                infile>>l>>r>>name;
-                if(l<=id&&id<=r)
-                    player_card[i].push_back(name);
-            }
-            infile.seekg(0,std::ios::beg);
-        }
-        infile.close();
+        players[i].load_cards(infile,5);
+        infile.seekg(0,std::ios::beg);
     }
-}
-
-void cd(int player_id)
-{
-    auto t=find(player_card[player_id].begin(),player_card[player_id].end(),"cd");
-    if(t!=player_card[player_id].end())
-    {
-        player_card[player_id].erase(t);
-        cout<<dire.name[player_index[player_id]]<<endl;
-        for(int i=0;i<dire.dire[player_index[player_id]].size();i++)
-        {
-            if(i==dire.dire[player_index[player_id]].size()-1)
-            {
-                cout<<"└──"<<serial_num[i+1]<<dire.name[dire.dire[player_index[player_id]][i]]<<endl;
-            }
-            else
-            {
-                cout<<"├──"<<serial_num[i+1]<<dire.name[dire.dire[player_index[player_id]][i]]<<endl;
-            }
-        }
-        while(1)
-        {
-            cout<<"请选择路径编号（如要后退输入..）：";
-            string temp;
-            cin>>temp;
-            if(temp=="..")
-            {
-                player_path[player_id].pop_back();
-                break;
-            }
-            int folder_id=temp[0]-'0';
-            if(folder_id>dire.dire[player_index[player_id]].size())
-            {
-                wostringstream temp_massage;
-                temp_massage<<L"没有这个路径哦";
-                wostringstream temp_title;
-                temp_title<<player_id<<L" 号玩家出牌阶段";
-                MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
-            }
-            player_path[player_id].push_back(dire.dire[player_index[player_id]][folder_id-1]);
-            player_index[player_id]=dire.dire[player_index[player_id]][folder_id-1];
-            break;
-        }
-    }
-    else
-    {
-        wostringstream temp_massage;
-        temp_massage<<L"您没有 cd 这张牌哦";
-        wostringstream temp_title;
-        temp_title<<player_id<<L" 号玩家出牌阶段";
-        MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
-    }
-}
-
-void mkdir(int player_id)
-{
-    auto t=find(player_card[player_id].begin(),player_card[player_id].end(),"mkdir");
-    if(t!=player_card[player_id].end())
-    {
-        player_card[player_id].erase(t);
-        string temp="foldler";temp+='0'+(++folder_tot);
-        dire.mkdir(player_index[player_id],temp);
-    }
-    else
-    {
-        wostringstream temp_massage;
-        temp_massage<<L"您没有 mkdir 这张牌哦";
-        wostringstream temp_title;
-        temp_title<<player_id<<L" 号玩家出牌阶段";
-        MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
-    }
+    infile.close();
 }
 
 void play()//main
 {
     while(player_sum)//if more than one player survives
     {
-        for(int i=1;i<=4;i++)
+        for(int player_id=1;player_id<=4;player_id++)
         {
             system("cls");//clean the screen
-            //pop up window
             wostringstream temp_massage;
-            temp_massage<<L"请除 "<<i<<L" 号以外玩家背身";
+            temp_massage<<L"请除 "<<player_id<<L" 号以外玩家背身";
             wostringstream temp_title;
-            temp_title<<i<<L" 号玩家出牌阶段";
+            temp_title<<player_id<<L" 号玩家出牌阶段";
             MessageBoxW(NULL,temp_massage.str().c_str(),temp_title.str().c_str(),MB_OK);
+            Player& player=players[player_id];
             while(1)
             {
-                //print player's path
-                dire.print();
-                cout<<i<<" 号玩家当前路径: "<<endl;
-                cout<<"Home";
-                for(int j=1;j<player_path[i].size();j++)
-                    cout<<"/"<<dire.name[player_path[i][j]];
-                cout<<endl;
-                //print player's card
-                cout<<i<<" 号玩家的手牌: "<<endl;
-                for(auto j=player_card[i].begin();j!=player_card[i].end();j++)
-                    cout<<"  "<<*j<<endl;
-                    
-                //play a hand
+                player.display(dire);
                 cout<<"您要打出（输入 0 结束回合）: ";
                 wstring temp;
                 wcin>>temp;
                 if(temp==L"0")
                     break;
                 if(temp==L"cd")
-                    cd(i); 
+                    player.cd(dire);
                 if(temp==L"mkdir")
-                    mkdir(i);    
+                    player.mkdir(dire);
             }
         }
     }
